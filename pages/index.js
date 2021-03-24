6c10ad4a2f2fb1500c5fa1629be7464747679e86
栗田学 <snoopy.mk90@gmail.com>
2021-03-24 20:55:58 +0900

    first-commit

---
+++ pages/index.js
@@ -0,0 +1,49 @@
+import Head from "next/head";
+import Link from "next/link";
+import Date from "../components/date";
+import Layout from "../components/layout";
+import { getSortedPostsData } from "../lib/posts";
+import utilStyles from "../styles/utils.module.css";
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head></Head>
+      <section className={utilStyles.headingMd}></section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1Px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+// export async function getStaticProps() {
+//   const allPostsData = getSortedPostsData();
+//   return {
+//     props: {
+//       allPostsData,
+//     },
+//   };
+// }
+
+export async function getServerSideProps(context) {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
